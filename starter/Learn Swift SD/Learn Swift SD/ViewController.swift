b9de329dfe7f386ebe706f05c25cb8aec4858910
Guillermo Alvarez <galvarezcity@me.com>
2017-06-06 17:51:22 -0700

    added starter

---
+++ starter/Learn Swift SD/Learn Swift SD/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Learn Swift SD
+//
+//  Created by Guillermo Alvarez on 6/6/17.
+//  Copyright © 2017 LearnSwiftSD. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
